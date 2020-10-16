424eae16446be022bdb748fce891ab1a3fcdf2f0
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-10-16 17:37:47 -0300

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import "./index.css";
 import App from "./App";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 //Se crea el cliente que apunta a la API
